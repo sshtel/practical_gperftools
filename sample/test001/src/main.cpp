adf254416609eb0956674e6edb157d2c337edf49
sshtel <sshtel@sshtel-Samsung-DeskTop-System.(none)>
2014-10-28 14:01:08 +0900

    remove unnecessary code

--- sample/test001/src/main.cpp
+++ sample/test001/src/main.cpp
@@ -1,51 +1,13 @@
 #include <iostream>
-
-
-#ifdef _USE_GPERFTOOLS
-#include <gperftools/heap-checker.h>
-#include <google/profiler.h>
-#include <google/heap-profiler.h>
-#include <google/heap-checker.h>
-#endif
-
 #include "allocate_release.h"
-
-void leak(){
-    int *a = new int[100];
-}
-
-void leak2(){
-    int *a = new int[100];
-}
-
-
-
-void noleak(){
-    int *a = new int[100];
-    delete a;
-
-}
 
 
 int main(int argc, char *argv[]){
 
-//    ::ProfilerStart("gtest.pprof");
-//    ::HeapProfilerStart("gtest.hprof");
-
-
     std::cout << "start program" << std::endl;
-    //HeapLeakChecker heap_checker("test_foo");
     {
         while(1)
         {
-            /*
-            int *a = new int[10];
-            delete a;
-            noleak();
-            leak();
-            noleak();
-            leak2();
-            */
             std::cout << "ready..." << std::endl;
             sleep(20);
             std::cout << "go..." << std::endl;
@@ -61,7 +23,6 @@ int main(int argc, char *argv[]){
                 release();
                 sleep(3);
             }
-            
 
             for(int i=0; i<10; i++){
                 allocate(1024*1024*4);
@@ -72,26 +33,8 @@ int main(int argc, char *argv[]){
                 release();
                 sleep(3);
             }
-
-
-
         }
     }
-
-    /*
-    if (!heap_checker.NoLeaks()) 
-    { 
-            std::cout << "leak!!!!!!! \n" << std::endl;
-    }
-    */
-
     std::cout << "return.." << std::endl;
-
-
-//    HeapLeakChecker::NoGlobalLeaks();
-//    ::HeapProfilerStop();
-//    ::ProfilerFlush();
-//    ::ProfilerStop();
-
     return 0;
 }
--- sample/test002/src/main.cpp
+++ sample/test002/src/main.cpp
@@ -1,23 +1,11 @@
 #include <iostream>
 
-
-#ifdef _USE_GPERFTOOLS
-#include <gperftools/heap-checker.h>
-#include <google/profiler.h>
-#include <google/heap-profiler.h>
-#include <google/heap-checker.h>
-#endif
-
 #include "allocate_release.h"
 
 
 int main(int argc, char *argv[]){
 
-//    ::ProfilerStart("test002.pprof");
-//    ::HeapProfilerStart("test002.hprof");
-
     std::cout << "start program" << std::endl;
-    //HeapLeakChecker heap_checker("test_foo");
     {
         while(1)
         {
@@ -41,21 +29,6 @@ int main(int argc, char *argv[]){
             }
         }
     }
-
-    /*
-    if (!heap_checker.NoLeaks()) 
-    { 
-            std::cout << "leak!!!!!!! \n" << std::endl;
-    }
-    */
-
     std::cout << "return.." << std::endl;
-
-
-//    HeapLeakChecker::NoGlobalLeaks();
-//    ::HeapProfilerStop();
-//    ::ProfilerFlush();
-//    ::ProfilerStop();
-
     return 0;
 }
